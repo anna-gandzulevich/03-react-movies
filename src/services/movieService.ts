fe36b823b9561cc960b8a8f9b6eeff66180a1bd1
Anna Gandzulevich <gandzulevich@gmail.com>
2025-06-14 18:44:33 +0300

    check 1

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import { type Movie } from '../types/movie.ts';
+import axios from "axios";
+import { type Movie } from "../types/movie.ts";
 
 interface MoviesResponse {
   results: Movie[];
@@ -8,14 +8,14 @@ interface MoviesResponse {
 export default async function fetchMovies(
   inputQuery: string
 ): Promise<Movie[]> {
-  const apiKey = import.meta.env.VITE_API_KEY;
+  const apiKey = import.meta.env.VITE_TMDB_TOKEN;
 
   const response = await axios.get<MoviesResponse>(
-    'https://api.themoviedb.org/3/search/movie',
+    "https://api.themoviedb.org/3/search/movie",
     {
       params: { query: inputQuery },
       headers: {
-        accept: 'application/json',
+        accept: "application/json",
         Authorization: `Bearer ${apiKey}`,
       },
     }
